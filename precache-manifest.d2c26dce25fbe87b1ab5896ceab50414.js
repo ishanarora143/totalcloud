9d753148b09eada848cfc628e2dffa9c620ca2c9
ishan arora <eshanarora00001@gmail.com>
2019-10-04 04:08:05 +0530

    Updates

---
+++ precache-manifest.d2c26dce25fbe87b1ab5896ceab50414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b819b01357a079591af4df76708570e8",
+    "url": "/totalcloud/index.html"
+  },
+  {
+    "revision": "73a5fbc8214beb2f25db",
+    "url": "/totalcloud/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4b6c5b1f7349d3010ba1",
+    "url": "/totalcloud/static/js/2.81501cf2.chunk.js"
+  },
+  {
+    "revision": "73a5fbc8214beb2f25db",
+    "url": "/totalcloud/static/js/main.141c8986.chunk.js"
+  },
+  {
+    "revision": "68db164abc1c1fbfbeb8",
+    "url": "/totalcloud/static/js/runtime-main.5d0634ba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/totalcloud/static/media/logo.25bf045c.svg"
+  }
+]);
